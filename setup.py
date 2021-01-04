13e0477c0262b28e000d2debe939f87efbe6aa66
sylvia-ytchen <67954609+sylvia-ytchen@users.noreply.github.com>
2021-01-04 14:30:21 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'PYNQ_webcam2hdmi_face_detect',
+    packages = ['webcam2hdmi_face_detect'],
+    version = '1.0',
+    description = 'PYNQ webcam2hdmi face detect',
+    author = 'cute',
+    author_email = 'design@soinc.com.tw',
+    url = 'https://github.com/silicon-optronics-inc/PYNQ_webcam2hdmi_face_detect',
+    download_url = 'https://github.com/silicon-optronics-inc/PYNQ_webcam2hdmi_face_detect',
+)
